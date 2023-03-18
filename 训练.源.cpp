132fbe87c6ac74037076c82ce31e2e3548974a0b
Mebius-xy <117964122+Mebius-xy@users.noreply.github.com>
2023-03-18 16:51:32 +0800

    Update 训练.源.cpp

--- 训练.源.cpp
+++ 训练.源.cpp
@@ -18,3 +18,38 @@ int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
     *returnSize = 0;
     return NULL;
 }
+---------------------
+double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Size) {
+    double returndata = 0;
+    int* rear = (int *)malloc(sizeof(int) * (nums1Size + nums2Size));
+    for (int i = 0; i < nums1Size; i++)
+    {
+        *(rear + i) = *(nums1 + i);
+    }
+    for (int j = 0; j < nums2Size; j++)
+    {
+        *(rear + nums1Size + j) = *(nums2 + j);
+    }
+    for (int i = 1; i < (nums1Size + nums2Size); i++)
+    {
+        for (int j = 0; j < (nums1Size + nums2Size - i); j++)
+        {
+            if (*(rear + j) > *(rear + j + 1))
+            {
+                int temp = *(rear + j);
+                *(rear + j) = *(rear + j + 1);
+                *(rear + j + 1) = temp;
+            }
+        }
+    }
+    if ((nums1Size + nums2Size) % 2 == 0)
+    {
+        returndata = (*(rear + (nums1Size + nums2Size) / 2) + *(rear + ((nums1Size + nums2Size) / 2 )- 1)) / 2;//这一句代码执行后得不出想要的结果，为什么？
+    }
+    else
+    {
+        returndata = *(rear + (nums1Size + nums2Size - 1) / 2);
+    }
+    free(rear);
+    return returndata;
+}
